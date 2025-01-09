ac3d38427f74af85dd8d581413c796175baa18e6
Tejas Tirthapura <tirthapuratejas@gmail.com>
2025-01-08 21:39:16 -0800

    refactored code to use classes instead of function

--- script.js
+++ script.js
@@ -1,94 +1,97 @@
-let myLibrary = [];
-
-function Book(title, author, pages) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
+class Book {
+    constructor(title, author, pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
 }
 
-function addBookToLibrary(book) {
-    myLibrary.push(book);
-    showBooks();
-}
+class Library {
+    constructor() {
+        this.dialog = document.querySelector("dialog");
+        this.showButton = document.querySelector("dialog + button");
+        this.submitButton = document.querySelector("dialog #submit");
+        this.libraryContainer = document.querySelector(".library");
+        
+        
+        // "Show the dialog" button opens the dialog modally
+        this.showButton.addEventListener("click", () => {
+            this.dialog.showModal();
+        });
+        
+        this.submitButton.addEventListener("click", (e) => {
+            e.preventDefault()
+            if (this.addNewBook()) {
+                document.querySelector("#title").value = "";
+                document.querySelector("#author").value = "";
+                document.querySelector("#pages").value = "";
+                this.dialog.close();
+            }
+        });
 
-function showBooks() {
-    const libraryContainer = document.querySelector(".library");
-    libraryContainer.textContent = "";
+        this.myLibrary = [];
+    }
 
-    for (const book of myLibrary) {
-        const bookContainer = document.createElement("div");
-        bookContainer.classList.add("book");
-
-        for (const key in book) {
-            const keyContainer = document.createElement("div");
-            keyContainer.classList.add(key);
-            keyContainer.textContent = book[key];
-            bookContainer.appendChild(keyContainer);
+    showBooks() {
+        this.libraryContainer.textContent = "";
+    
+        for (const book of this.myLibrary) {
+            const bookContainer = document.createElement("div");
+            bookContainer.classList.add("book");
+    
+            for (const key in book) {
+                const keyContainer = document.createElement("div");
+                keyContainer.classList.add(key);
+                keyContainer.textContent = book[key];
+                bookContainer.appendChild(keyContainer);
+            }
+    
+            bookContainer.querySelector(".author").textContent = "Author: " + bookContainer.querySelector(".author").textContent;
+            bookContainer.querySelector(".pages").textContent = "Pages: " + bookContainer.querySelector(".pages").textContent;
+    
+            const deleteButton = document.createElement("button");
+            deleteButton.textContent = "Delete";
+            deleteButton.id = book.title.replaceAll(" ", "_");
+            deleteButton.addEventListener("click", () => {
+                this.myLibrary = this.myLibrary.filter((book) => book.title.replaceAll(" ", "_") !== deleteButton.id);
+                this.showBooks();
+            }); 
+            bookContainer.appendChild(deleteButton);
+    
+            this.libraryContainer.append(bookContainer);
         }
+    }
 
-        bookContainer.querySelector(".author").textContent = "Author: " + bookContainer.querySelector(".author").textContent;
-        bookContainer.querySelector(".pages").textContent = "Pages: " + bookContainer.querySelector(".pages").textContent;
+    addBook(book) {
+        this.myLibrary.push(book);
+        this.showBooks();
+    }
 
-        const deleteButton = document.createElement("button");
-        deleteButton.textContent = "Delete";
-        deleteButton.id = book.title.replaceAll(" ", "_");
-        deleteButton.addEventListener("click", () => {
-            myLibrary = myLibrary.filter((book) => book.title.replaceAll(" ", "_") !== deleteButton.id);
-            showBooks();
-        }); 
-        bookContainer.appendChild(deleteButton);
-
-        libraryContainer.append(bookContainer);
+    addNewBook() {
+        const newTitle = document.querySelector("#title").value;
+        const newAuthor = document.querySelector("#author").value;
+        const newPages = document.querySelector("#pages").value;
+        if (!newTitle || !newAuthor || isNaN(newPages) || newPages < 0) {
+            alert("Please fill all fields correctly.");
+            return false;
+        }
+        const newBook = new Book(newTitle, newAuthor, newPages);
+        console.log(newBook);
+        this.addBook(newBook);
+        return true;
     }
 }
 
-addBookToLibrary(new Book("The Exaltation of Inana", "Enheduana", 50));
-addBookToLibrary(new Book("Gilgamesh", "Unknown", 210));
-addBookToLibrary(new Book("Genesis", "Unknown", 150));
-addBookToLibrary(new Book("Iliad", "Homer, tr. Lattimore", 704));
-addBookToLibrary(new Book("If Not, Winter: Fragments of Sappho", "Sappho, tr. Carson", 416));
-addBookToLibrary(new Book("Odyssey", "Homer, tr. Wilson", 560));
-addBookToLibrary(new Book("Oresteia", "Aeschylus, tr. Lattimore", 336));
-addBookToLibrary(new Book("Symposium", "Plato, trs. Nehamas and Woodruff", 128));
-addBookToLibrary(new Book("Aeneid", "Virgil, tr. Mandelbaum", 416));
-addBookToLibrary(new Book("Gospel of Luke", "Unknown", 60));
-addBookToLibrary(new Book("Gospel of John", "Unknown", 40));
-
-
-showBooks();    
-
-
-
-function addNewBook() {
-    const newTitle = document.querySelector("#title").value;
-    const newAuthor = document.querySelector("#author").value;
-    const newPages = document.querySelector("#pages").value;
-    if (!newTitle || !newAuthor || isNaN(newPages) || newPages < 0) {
-        alert("Please fill all fields correctly.");
-        return false;
-    }
-    const newBook = new Book(newTitle, newAuthor, newPages);
-    console.log(newBook);
-    addBookToLibrary(newBook);
-    return true;
-}
-
-const dialog = document.querySelector("dialog");
-const showButton = document.querySelector("dialog + button");
-const submitButton = document.querySelector("dialog #submit");
-
-
-// "Show the dialog" button opens the dialog modally
-showButton.addEventListener("click", () => {
-    dialog.showModal();
-});
-
-submitButton.addEventListener("click", (e) => {
-    e.preventDefault()
-    if (addNewBook()) {
-        document.querySelector("#title").value = "";
-        document.querySelector("#author").value = "";
-        document.querySelector("#pages").value = "";
-        dialog.close();
-    }
-});
+const myLibrary = new Library();
+myLibrary.addBook(new Book("The Exaltation of Inana", "Enheduana", 50));
+myLibrary.addBook(new Book("Gilgamesh", "Unknown", 210));
+myLibrary.addBook(new Book("Genesis", "Unknown", 150));
+myLibrary.addBook(new Book("Iliad", "Homer, tr. Lattimore", 704));
+myLibrary.addBook(new Book("If Not, Winter: Fragments of Sappho", "Sappho, tr. Carson", 416));
+myLibrary.addBook(new Book("Odyssey", "Homer, tr. Wilson", 560));
+myLibrary.addBook(new Book("Oresteia", "Aeschylus, tr. Lattimore", 336));
+myLibrary.addBook(new Book("Symposium", "Plato, trs. Nehamas and Woodruff", 128));
+myLibrary.addBook(new Book("Aeneid", "Virgil, tr. Mandelbaum", 416));
+myLibrary.addBook(new Book("Gospel of Luke", "Unknown", 60));
+myLibrary.addBook(new Book("Gospel of John", "Unknown", 40));
+myLibrary.showBooks();
